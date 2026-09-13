57316230f966091afc5b211cf434b0cfd83eae31
Kundan Goyary <kgoyary824@gmail.com>
2026-09-13 13:26:34 +0530

    Update reelio image import path

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -4,7 +4,7 @@ import ProjectCard from "../components/ProjectCard";
 // Import images
 import fitrackImg from "../assets/fitrack.png";
 import veloraImg from "../assets/velora.png";
-import reelioImg from "../assets/3.png";
+import reelioImg from "../assets/reelio.png";
 import budgetwiseImg from "../assets/budgetwise.png";
 import codeEditorImg from "../assets/code-editor.png";
 import shopmateImg from "../assets/shopmate.png";
